a17ec273580b82513ade7d9ba2cc64f93fe41db7
Sergio Cervantes <50383374+Zappaware@users.noreply.github.com>
2024-01-20 14:33:25 -0600

    Add projects of the solution to the project

---
+++ MagicalInheritance/Archmage.cs
@@ -0,0 +1,20 @@
+﻿namespace MagicalInheritance;
+
+public class Archmage : Mage
+{
+    public Archmage(string title) : base(title)
+    {
+    }
+    
+    public override Storm CastRainStorm()
+    {
+        return new Storm("rain", true, Title);
+    }
+    
+#pragma warning disable CS0108 // El miembro oculta el miembro heredado. Falta una contraseña nueva
+    public Storm CastLightningStorm()
+#pragma warning restore CS0108 // El miembro oculta el miembro heredado. Falta una contraseña nueva
+    {
+        return new Storm("lightning", true, Title);
+    }
+}
